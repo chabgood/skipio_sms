ce0dbb129f51c07ebe1c6486b1301c16588c34fe
Chris Habgood <chabgood@gmail.com>
2017-10-16 09:49:52 -0500

    forgot to update the name of the test after the method change

--- spec/models/message.rb
+++ spec/models/message.rb
@@ -1,5 +1,5 @@
 describe Message do
-  context "#send" do
+  context ".deliver" do
     it "be successful" do
       message = Message.new(id: '6a91244e-60aa-43fa-ac92-bd2025a86d49', message: 'my shiney new text')
       expect(message.deliver).to be_success
